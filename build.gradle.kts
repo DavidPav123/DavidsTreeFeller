9ab646de2da12886760d949dcfcffc3df8a44149
DavidPav123 <davidpavlicek123@gmail.com>
2023-07-23 14:29:18 -1000

    Editing  project

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,16 @@ plugins {
 }
 
 group = "io.github.davidpav123.treefeller"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
-
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
 }
 
-tasks.test {
-    useJUnitPlatform()
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
--- src/main/java/io/github/davidpav123/treefeller/TreeFeller.java
+++ src/main/java/io/github/davidpav123/treefeller/TreeFeller.java
@@ -1,6 +1,6 @@
 package io.github.davidpav123.treefeller;
 
-public class Main {
+public class TreeFeller {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
